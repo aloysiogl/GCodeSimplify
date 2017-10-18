ad54f29aaecb6bb13c2de843e398384d9c03b5b7
Unknown <aloysiogl@gmail.com>
2017-10-18 20:29:53 -0200

    Finished gCode altering

--- gcodeSimplify.cpp
+++ gcodeSimplify.cpp
@@ -4,10 +4,13 @@
 #include <vector>
 #include <cstring>
 #include <cstdio>
+#include <fstream>
+
+#define N_ERR 7
 
 using namespace std;
 
-// folder must end with "/", e.g. "D:/images/"
+// Folder must end with "/", e.g. "D:/images/"
 vector<string> get_all_files_full_path_within_folder(string folder)
 {
     vector<string> names;
@@ -30,6 +33,74 @@ vector<string> get_all_files_full_path_within_folder(string folder)
     return names;
 }
 
+//Correction function which suggests corrections
+string correctionFunction(string line, int &err){
+    bool correct = false;
+    switch (err){
+    //Removing Z0
+    case 0:
+        if (line.find("G53 G0 Z0.") != string::npos){
+            line = "";
+            correct = true;
+        }
+    break;
+    //Removing three lines
+    case 1:
+        if (line.find("M9") != string::npos){
+            line = "";
+            correct = true;
+        }
+    break;
+    case 2:
+        if (true){
+            line = "";
+            correct = true;
+        }
+    break;
+    case 3:
+        if (true){
+            line = "";
+            correct = true;
+        }
+    break;
+    //Adding Z20
+    case 4:
+        if (line.find("G54") != string::npos){
+            line = "G54\nG0 Z20.\n";
+            correct = true;
+        }
+    break;
+    //Removing H
+    case 5:
+        if (line.find("H") != string::npos){
+            line = line.substr(0,line.size()-2)+'\n';
+            correct = true;
+        }
+    break;
+    //Removing M9
+    case 6:
+        if (line.find("M9") != string::npos){
+            line = "";
+            correct = true;
+        }
+    break;
+    //Removing Z0
+    case 7:
+        if (line.find("G53 Z0.") != string::npos){
+            line = "";
+            correct = true;
+        }
+    }
+
+    if (!correct){
+        return line + '\n';
+    }
+    else {
+        err++;
+        return line;
+    }
+}
+
 int main(){
     //Getting all the file names within the current directory
     vector<string> files;
@@ -50,13 +121,51 @@ int main(){
 
     files.clear();
 
+    //Creating vector of output names
+    vector<string> gCodeOutNames;
+
+    for (int i = 0; i < gCodeNames.size(); ++i){
+        string outName = ".\\out" + gCodeNames[i].substr(1,string::npos);
+        gCodeOutNames.push_back(outName);
+    }
+
+    //Creating output directory
+    CreateDirectory(".\\out",NULL);
+
+    //Counting errors
+    int nErr = 0;
+
     //Do the file correction to each gCode in the vector
     for (int i = 0; i < gCodeNames.size(); ++i){
+        ifstream gCodeOriginal(gCodeNames[i].c_str());
+        ofstream gCodeEdit;
+        gCodeEdit.open(gCodeOutNames[i].c_str());
 
+        string line;
+
+        int errorIndex = 0;
+
+        //Going through the files
+        while(getline(gCodeOriginal,line)){
+
+            line = correctionFunction(line, errorIndex);
+            gCodeEdit <<line;
+        }
+
+        if (errorIndex == N_ERR+1){
+            cout << "Generated "<< gCodeOutNames[i] << " ok!\n";
+        }
+
+        else {
+            cout << "Generated "<< gCodeOutNames[i] << N_ERR+1 - errorIndex <<" modifications couldn't be done.\n";
+            nErr++;
+        }
+
+        gCodeEdit.close();
     }
 
-    //Test
-    for (int i = 0; i < gCodeNames.size(); ++i){
-        cout << gCodeNames[i] << "\n";
-    }
+    cout << "All files processed with "<< nErr << " errors!\n";
+
+    //End of the program
+    system("pause");
 }
